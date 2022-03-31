9a5f8b37eac98403ef8ff1768538886eb2276288
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2022-03-30 21:17:33 -0300

    Move curve point arithmetic into their own modules

--- x25519.lua
+++ x25519.lua
@@ -5,82 +5,7 @@
 
 local expect = require "cc.expect".expect
 local fp     = require "ccryptolib.internal.fp"
-local random = require "ccryptolib.random"
-
-local unpack = unpack or table.unpack
-
-local function double(x1, z1)
-    local a = fp.add(x1, z1)
-    local aa = fp.square(a)
-    local b = fp.sub(x1, z1)
-    local bb = fp.square(b)
-    local c = fp.sub(aa, bb)
-    local x3 = fp.mul(aa, bb)
-    local z3 = fp.mul(c, fp.add(bb, fp.kmul(c, 121666)))
-    return x3, z3
-end
-
-local function step(dxmul, dx, x1, z1, x2, z2)
-    local a = fp.add(x1, z1)
-    local aa = fp.square(a)
-    local b = fp.sub(x1, z1)
-    local bb = fp.square(b)
-    local e = fp.sub(aa, bb)
-    local c = fp.add(x2, z2)
-    local d = fp.sub(x2, z2)
-    local da = fp.mul(d, a)
-    local cb = fp.mul(c, b)
-    local x4 = fp.square(fp.add(da, cb))
-    local z4 = dxmul(fp.square(fp.sub(da, cb)), dx)
-    local x3 = fp.mul(aa, bb)
-    local z3 = fp.mul(e, fp.add(bb, fp.kmul(e, 121666)))
-    return x3, z3, x4, z4
-end
-
-local function bits(str)
-    -- Decode.
-    local bytes = {str:byte(1, 32)}
-    local out = {}
-    for i = 1, 32 do
-        local byte = bytes[i]
-        for j = -7, 0 do
-            local bit = byte % 2
-            out[8 * i + j] = bit
-            byte = (byte - bit) / 2
-        end
-    end
-
-    -- Clamp.
-    out[256] = 0
-    out[255] = 1
-
-    -- We remove the 3 lowest bits since the ladder already multiplies by 8.
-    return {unpack(out, 4)}
-end
-
-local function ladder8(dxmul, dx, bits)
-    local x1 = fp.num(1)
-    local z1 = fp.num(0)
-
-    local z2 = fp.decode(random.random(32))
-    local x2 = dxmul(z2, dx)
-
-    -- Standard ladder.
-    for i = #bits, 1, -1 do
-        if bits[i] == 0 then
-            x1, z1, x2, z2 = step(dxmul, dx, x1, z1, x2, z2)
-        else
-            x2, z2, x1, z1 = step(dxmul, dx, x2, z2, x1, z1)
-        end
-    end
-
-    -- Multiply by 8 (double 3 times).
-    for _ = 1, 3 do
-        x1, z1 = double(x1, z1)
-    end
-
-    return fp.mul(x1, fp.invert(z1))
-end
+local mont   = require "ccryptolib.internal.curve25519"
 
 local mod = {}
 
@@ -92,7 +17,7 @@ local mod = {}
 function mod.publicKey(sk)
     expect(1, sk, "string")
     assert(#sk == 32, "secret key length must be 32")
-    return fp.encode(ladder8(fp.kmul, 9, bits(sk)))
+    return fp.encode(mont.ladder8(fp.kmul, 9, mont.bits(sk)))
 end
 
 --- Performs the key exchange.
@@ -106,7 +31,7 @@ function mod.exchange(sk, pk)
     assert(#sk == 32, "secret key length must be 32")
     expect(2, pk, "string")
     assert(#pk == 32, "public key length must be 32")
-    return fp.encode(ladder8(fp.mul, fp.decode(pk), bits(sk)))
+    return fp.encode(mont.ladder8(fp.mul, fp.decode(pk), mont.bits(sk)))
 end
 
 return mod
